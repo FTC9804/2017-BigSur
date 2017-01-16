4cd52cab130270add97caacfe85ac74c9a55b085
mleher1 <mleher1@hwemail.com>
2017-01-15 17:35:19 -0800

    New Auto, Blue addition
    Lining up on white line

--- LAMeet3/BLUESimpleAutoBeaconInFront.java
+++ LAMeet3/BLUESimpleAutoBeaconInFront.java
@@ -1,34 +1,96 @@
+//Version 1.0 coded Jan. 15, 2017 by Rylan and Marcus.  Designed to test the new autonomous concept
+//of driving forward, shooting, turning to 45, finding the white line, turning to 90
+//and then driving forward and pressing the beacon.  In version 1.0 we solely tested driving
+//forward, turning to 45, finding the white line, and turning to 90, which was accomplished.
+
+
+
+
+
+
+
+
+
+
+//package declaration
 package org.firstinspires.ftc.teamcode;
 
-
+//import statement
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "BLUESimpleAutoBeaconInFront", group = "AutoWithFunctions")
+@Autonomous(name = "BLUESimpleAutoBeaconInFront1.12", group = "AutoWithFunctions")
 //@Disabled
-public class BLUESimpleAutoBeaconInFront extends FunctionsForMeet3 {
+public class BLUESimpleAutoBeaconInFront extends FUNCTIONSDEC20 {
 
     public void runOpMode() throws InterruptedException {
+
+        //Configure motors, servos and sensors
         Configure();
 
+        //Wait until play button is pressed
         waitForStart();
 
-        calibrateGyro();
 
-        /*Driving code*/
+       //Calibrate the Gyro
+       calibrateGyro();
 
-        drive(22, .4, 0);
+        //Drive Forward 10 inches at .4 speed, maintaining a gyro heading of 0
+        drive(10, .4, 0);
 
-        shootAndLift(20, 2300, .95, .95);
+        //Run shootAndLift function for 20 seconds at a goal of 2300 rpm, with intake and elevator speeds of .95
+        //shootAndLift(20, 2300, .95, .95);
 
-        spinMove(-40);
+        //Move 45 degrees clockwise.  Set parameter to 36 degrees clockwise to combat overshoot in the gyro
+        spinMove(-36);
 
-        driveToOneWhiteLine();
 
-        spinMove(-85);
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
 
-        driveToTouch(0.25);
+        //Wait three seconds
+        while (timeTwo - timeOne <3)
+        {
+            timeTwo=this.getRuntime();
+        }
 
-        pressBeaconFrontBlue();
+        //Run driveToOneWhiteLineRight function
+        driveToOneWhiteLineRight();
+
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
+
+        //Run driveToOneWhiteLineRight function
+        while (timeTwo - timeOne <3)
+        {
+            timeTwo=this.getRuntime();
+        }
+
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
+
+        //Run driveToOneWhiteLineRight function
+        while (timeTwo - timeOne <3)
+        {
+            timeTwo=this.getRuntime();
+        }
+
+
+        //Move to 90 degrees clockwise.  Set parameter to 77 degrees clockwise to combat overshoot in the gyro
+        spinMove(-77);
+
+        //Run driveToTouch method, applying .25 power to the motors when doing so
+        //driveToTouch(0.25);
+
+        //Run pressBeaconFrontBlue method
+        //pressBeaconFrontBlue();
+
+
+
+
+
 
 
 
