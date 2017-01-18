7c6767760bfaaafd6156c5cc1f75f33ac87e5a82
mleher1 <mleher1@hwemail.com>
2017-01-17 18:20:27 -0800

    Use encoder and gyro to turn

--- LAMeet3/REDSimpleAutoBeaconInFront.java
+++ LAMeet3/REDSimpleAutoBeaconInFront.java
@@ -1,9 +1,15 @@
-
-
 //Version 1.0 coded Jan. 15, 2017 by Rylan and Marcus.  Designed to test the new autonomous concept
 //of driving forward, shooting, turning to 45, finding the white line, turning to 90
 //and then driving forward and pressing the beacon.  In version 1.0 we solely tested driving
 //forward, turning to 45, finding the white line, and turning to 90, which was accomplished.
+
+
+
+
+
+
+
+
 
 
 //package declaration
@@ -12,7 +18,7 @@ package org.firstinspires.ftc.teamcode;
 //import statement
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "REDSimpleAutoBeaconInFront1.204", group = "AutoWithFunctions")
+@Autonomous(name = "BLUESimpleAutoBeaconInFront1.46", group = "AutoWithFunctions")
 //@Disabled
 public class REDSimpleAutoBeaconInFront extends FUNCTIONSDEC20 {
 
@@ -28,14 +34,25 @@ public class REDSimpleAutoBeaconInFront extends FUNCTIONSDEC20 {
         //Calibrate the Gyro
         calibrateGyro();
 
-        //Drive Forward 10 inches at .4 speed, maintaining a gyro heading of 0
-        drive(10, .4, 0);
-
         //Run shootAndLift function for 20 seconds at a goal of 2300 rpm, with intake and elevator speeds of .95
         //shootAndLift(20, 2300, .95, .95);
 
-        //Move 45 degrees counterclockwise.  Set parameter to 36 degrees counterclockwise to combat overshoot in the gyro
-        spinMove(36);
+        drive(20, .2, 0);
+
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
+
+        //Wait three seconds
+        while (timeTwo - timeOne <1)
+        {
+            timeTwo=this.getRuntime();
+        }
+
+        //Move 45 degrees clockwise.  Set parameter to 36 degrees clockwise to combat overshoot in the gyro
+        //spinMove(-32);
+
+        encoderTurnCounterClockwise(.55, .28);
 
 
         //Set timeOne and timeTwo to the current run time
@@ -61,6 +78,14 @@ public class REDSimpleAutoBeaconInFront extends FUNCTIONSDEC20 {
             timeTwo=this.getRuntime();
         }
 
+
+
+        //Move to 90 degrees clockwise.  Set parameter to 73 degrees clockwise to combat overshoot in the gyro
+        //spinMove(-87);
+
+        encoderTurnCounterClockwise(.8, .28);
+
+
         //Set timeOne and timeTwo to the current run time
         timeTwo=this.getRuntime();
         timeOne=this.getRuntime();
@@ -71,20 +96,33 @@ public class REDSimpleAutoBeaconInFront extends FUNCTIONSDEC20 {
             timeTwo=this.getRuntime();
         }
 
+        spinMove(86);
 
-        //Move to 90 degrees counterclockwise.  Set parameter to 73 degrees counterclockwise to combat overshoot in the gyro
-        spinMove(73);
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
 
-
-
-
-
+        //Run driveToOneWhiteLineRight function
+        while (timeTwo - timeOne <3)
+        {
+            timeTwo=this.getRuntime();
+        }
 
         //Run driveToTouch method, applying .25 power to the motors when doing so
-        //driveToTouch(0.12);
+        driveToTouch(0.1);
 
-        //Run pressBeaconFrontRed method
-        //pressBeaconFrontRed();
+        //Set timeOne and timeTwo to the current run time
+        timeTwo=this.getRuntime();
+        timeOne=this.getRuntime();
+
+        //Run driveToOneWhiteLineRight function
+        while (timeTwo - timeOne <3)
+        {
+            timeTwo=this.getRuntime();
+        }
+
+        //Run pressBeaconFrontBlue method
+        pressBeaconFrontRed();
 
 
 
