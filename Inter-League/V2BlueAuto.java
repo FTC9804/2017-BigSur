d59c279454bf552d8aaee96b78a52f526042ab1f
mleher1 <mleher1@hwemail.com>
2017-02-04 23:24:43 -0800

    Comments, delete unused code

--- Inter-League/V2BlueAuto.java
+++ Inter-League/V2BlueAuto.java
@@ -1,18 +1,6 @@
-
-
-//Version 1.0 coded Jan. 15, 2017 by Rylan and Marcus.  Designed to test the new autonomous concept
-//of driving forward, shooting, turning to 45, finding the white line, turning to 90
-//and then driving forward and pressing the beacon.  In version 1.0 we solely tested driving
-//forward, turning to 45, finding the white line, and turning to 90, which was accomplished.
-
-
-
-
-
-
-
-
-
+//Version 1.0 coded Feb. 4, 2017 by Steve, Etienne and Marcus.
+//Designed to test the autonomous concept
+//of shooting two balls and pressing two beacons
 
 //package declaration
 package org.firstinspires.ftc.teamcode;
@@ -20,9 +8,10 @@ package org.firstinspires.ftc.teamcode;
 //import statement
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "V2BLUEAUTO1.01", group = "AutoWithFunctions")
+@Autonomous(name = "V2BLUE", group = "AutoWithFunctions")
+
 //@Disabled
-public class V2BlueAuto extends FunctionsForILT {
+public class V2BlueAuto extends FuctionsForILTNew {
 
     public void runOpMode() throws InterruptedException {
 
@@ -32,59 +21,41 @@ public class V2BlueAuto extends FunctionsForILT {
         //Wait until play button is pressed
         waitForStart();
 
+        //Set shooter to variable shooterPower
         shooter.setPower(shooterPower);
 
-        //Calibrate the Gyro
-        //calibrateGyro();
-
+        //Drive back for 20 inches at .4 power
         driveBack(20, .4);
 
+        //Shoot and Lift function: 2800 rpm, .95 intake
+        shootAndLift(2800, .95);
 
-        shootAndLift(10, 2800, .95, .95);
-
+        //Turn cw .35 rotations at .25 power
         encoderTurnClockwise(.78, .25);
 
+        //Drive at -.8 power until left ods sees adequate white light
         driveToWhiteLineLeft(-.8);
 
+        //Drive back 15 inches at .8 power
         driveBack(15, .8);
 
+        //Turn ccw .15 rotations at .25 power
         encoderTurnCounterClockwise(.15, .25);
 
+        //Drive back, applying higher power to the left side until the right ods sees adequate white light
         driveToWhiteLineLeftRightSideFaster(-1);
 
+        //Run press beacon side blue method at -.15 power
         pressBeaconSideBlue(-.15);
 
-        encoderTurnCounterClockwise(.25, .25);
+        //Turn ccw .2 rotations at .25 power
+        encoderTurnCounterClockwise(.2, .25);
 
+        //Run driveMoreRight method for 24 inches at .8 power
         driveMoreRight(24, .8);
 
+        //Run press beacon side blue method at .175 power
         pressBeaconSideBlue(.175);
-
-
-
-//        spinMove(-25);
-//
-//
-//        driveMoreRightBack(40, .25, 0);
-//
-//
-//        lineUpFasterRightBack();
-//
-//
-//        pressBeaconFrontBlueNew(true);
-//
-//
-//        driveMoreRight (29, .5, 0);
-//
-//
-//        lineUpFasterRight();
-//
-//
-//        pressBeaconFrontBlueNew(false);
-//
-//        spinMove(-18);
-//
-//        drive(33, .6);
     }
 
 }
