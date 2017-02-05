324f85e41d06f4d106cae86928e70312fa75db70
mleher1 <mleher1@hwemail.com>
2017-02-04 23:24:06 -0800

    Comments, delete unused code

--- Inter-League/V2RedAuto.java
+++ Inter-League/V2RedAuto.java
@@ -1,8 +1,6 @@
-/* V2 created for the new Drogon robot with adaptations to the driving with the
- * redesigned west coast drive and updated chassis.
- * Separate codes for red and blue
- */
-
+//Version 1.0 coded Feb. 4, 2017 by Steve, Etienne and Marcus.
+//Designed to test the autonomous concept
+//of shooting two balls and pressing two beacons
 
 //package declaration
 package org.firstinspires.ftc.teamcode;
@@ -10,50 +8,54 @@ package org.firstinspires.ftc.teamcode;
 //import statement
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "V2REDAUTO", group = "AutoWithFunctions")
+
+@Autonomous(name = "V2RED", group = "AutoWithFunctions")
+
 //@Disabled
-public class V2RedAuto extends FunctionsForILT {
+public class V2RedAuto extends FuctionsForILTNew {
 
     public void runOpMode() throws InterruptedException {
-//Configure motors, servos and sensors
+
+        //Configure motors, servos and sensors
         Configure();
 
         //Wait until play button is pressed
         waitForStart();
 
-        //Calibrate the Gyro
-        calibrateGyro();
+        //Set shooter to variable shooterPower
+        shooter.setPower(shooterPower);
 
+        //Drive back for 20 inches at .4 power
+        driveBack(20, .4);
 
-        driveBack(20, .2);
+        //Shoot and Lift function: 2800 rpm, .95 intake
+        shootAndLift(2800, .95);
 
+        //Turn ccw .35 rotations at .25 power
+        encoderTurnCounterClockwise(.35, .25);
 
-        shootAndLift(10, 2025, .95, .95);
+        //Drive at -.5 power until right ods sees adequate white light
+        driveToWhiteLineRight(-.5);
 
+        //Drive back 7 inches at .8 power
+        driveBack(7, .8);
 
-        spinMove(25);
+        //Turn cw .22 rotations at .25 power
+        encoderTurnClockwise(.22, .25);
 
+        //Drive back, applying higher power to the right side until the left ods sees adequate white light
+        driveToWhiteLineRightLeftSideFaster(-1);
 
-        driveMoreLeftBack(40, .25, 0);
+        //Run press beacon side red method at -.15 power
+        pressBeaconSideRed(-.15);
 
+        //Turn cw .2 rotations at .25 power
+        encoderTurnClockwise(.2, .25);
 
-        lineUpFasterLeftBack();
+        //Run driveMoreLeft method for 24 inches at .8 power
+        driveMoreLeft(24, .8);
 
-
-        pressBeaconFrontRedNew(true);
-
-
-        driveMoreRight (29, .5, 0);
-
-
-        lineUpFasterRight();
-
-
-        pressBeaconFrontBlueNew(false);
-
-        spinMove(-18);
-
-
-        drive(33, .6);
+        //Run press beacon side red method at .175 power
+        pressBeaconSideRed(.175);
     }
 }
