2ba174997e4c5757be7317ee6bf88efb689dc674
mleher1 <mleher1@hwemail.com>
2017-02-10 11:15:41 -0800

    Update V2BlueAuto.java

--- LAChampionships/V2BlueAuto.java
+++ LAChampionships/V2BlueAuto.java
@@ -21,41 +21,24 @@ public class V2BlueAuto extends FuctionsForILTNew {
         //Wait until play button is pressed
         waitForStart();
 
-        //Set shooter to variable shooterPower
-        shooter.setPower(shooterPower);
+        calibrateGyro();
 
-        //Drive back for 20 inches at .4 power
-        driveBack(20, .4);
+        driveBack(20, .4, 0);
 
-        //Shoot and Lift function: 2800 rpm, .95 intake
-        shootAndLift(2800, .95);
+        spinMove(75);
 
-        //Turn cw .35 rotations at .25 power
-        encoderTurnClockwise(.78, .25);
+        spinMove(90);
 
-        //Drive at -.8 power until left ods sees adequate white light
-        driveToWhiteLineLeft(-.8);
+        driveBack(20, .4, 0);
 
-        //Drive back 15 inches at .8 power
-        driveBack(15, .8);
+        driveToTouch(.1);
 
-        //Turn ccw .15 rotations at .25 power
-        encoderTurnCounterClockwise(.15, .25);
+        pivot (3, .2, false);
 
-        //Drive back, applying higher power to the left side until the right ods sees adequate white light
-        driveToWhiteLineLeftRightSideFaster(-1);
+        drive (25, .3, 185);
 
-        //Run press beacon side blue method at -.15 power
-        pressBeaconSideBlue(-.15);
+        pressBeaconSideBlue(.15);
 
-        //Turn ccw .2 rotations at .25 power
-        encoderTurnCounterClockwise(.2, .25);
-
-        //Run driveMoreRight method for 24 inches at .8 power
-        driveMoreRight(24, .8);
-
-        //Run press beacon side blue method at .175 power
-        pressBeaconSideBlue(.175);
     }
 
 }
