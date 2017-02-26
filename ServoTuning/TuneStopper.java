ccfa392546e880db0601f8f72ca5d503dc170077
mleher1 <mleher1@hwemail.com>
2017-02-25 19:07:34 -0800

    stopper tuning
    new values are .3 for stopping and .6 for out of the way

--- ServoTuning/TuneStopper.java
+++ ServoTuning/TuneStopper.java
@@ -6,28 +6,53 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by stevecox on 2/24/17.
- * Purpose of the code is to allow the ability to tune stopper servo
+ *
+ * Created by stevecox on 2/25/17.
+ *
+ * Purpose of the code is to allow the ability to
+ *  drive back over a specified distance,
+ *  shoot, and keep driving
+ *
  */
 
-@Autonomous(name = "stopper", group = "ServoTuning")
+@Autonomous(name = "Auto Shoot Options", group = "AutoWithFunctions")
 //@Disabled
-public class TuneStopper extends FunctionsForLA {
+public class AutoShootOptions extends FunctionsForLA {
 
     //OVERALL VARIABLES
-    double timeOne, timeTwo, stopperStartPosition = 0, stopperEndPosition = 0.5;
-    int stepInMenu = 1;
-    boolean notAllChosen = true, choiceNotSelected = true, goBack = false, startPositionNotSelected = true, endPositionNotSelected = true;
+    double timeOne, timeTwo, wait, distance1, distance2, power;
+    int stepInMenu, heading;
+    boolean notAllChosen, choiceNotSelected, goBack,
+            waitTimeNotSelected, distance1NotSelected,
+            distance2NotSelected , motorPowerNotSelected;
 
     public void runOpMode() throws InterruptedException
     {
+        timeOne = 0;
+        timeTwo =0;
+        wait = 0;
+        distance1 = 0;
+        distance2 = 0;
+        power = 0;
+        stepInMenu = 1;
+        heading = 0;
+        notAllChosen = true;
+        choiceNotSelected = true;
+        goBack = false;
+        waitTimeNotSelected = true;
+        distance1NotSelected = true;
+        distance2NotSelected = true;
+        motorPowerNotSelected = true;
+
 
         /**
          *
          * Order of Auto Menu
          *
-         * 1. Set start position
-         * 2. Set end position
+         * 1. Set wait time
+         * 2. Set first distance
+         * 3. Set second distance
+         * 4. Set driving power
          *
          */
 
@@ -38,93 +63,128 @@ public class TuneStopper extends FunctionsForLA {
 
             goBack = false;
             if (stepInMenu == 1) {
-                setStartPosition();
+                setWaitTime();
             }
             delayShort();
-
 
             goBack = false;
             if (stepInMenu == 2) {
-                setEndPosition();
+                setDistance1ToTravel();
             }
             delayShort();
 
+            goBack = false;
+            if (stepInMenu == 3) {
+                setDistance2ToTravel();
+            }
+            delayShort();
 
-            if (!startPositionNotSelected && !endPositionNotSelected) {
+            goBack = false;
+            if (stepInMenu == 4) {
+                setMotorPower();
+            }
+
+            if (!waitTimeNotSelected && !distance1NotSelected && !distance2NotSelected && !motorPowerNotSelected) {
                 notAllChosen = false;
             } else {
                 notAllChosen = true;
             }
-
         }
 
-        telemetry.addLine("HAVE A FANTASTIC DAY");
+        telemetry.addLine("GET READY TO START");
+        telemetry.addLine(" ");
+        telemetry.addData("Delay time = ", "%.1f", wait);
+        telemetry.addData("Distance 1 = ", "%.1f", distance1);
+        telemetry.addData("Distance 2 = ", "%.1f", distance2);
+        telemetry.addData("DrivePower = ", "%.2f", power);
         telemetry.update();
-        delayLong();
-
 
         //Wait until play button is pressed
         waitForStart();
 
-        stopperStartPosition = Range.clip(stopperStartPosition, 0, 1);
-        stopperEndPosition = Range.clip(stopperEndPosition, 0, 1);
-
-        telemetry.clearAll();
-        telemetry.addData("Gamepad1 x for in position", stopperStartPosition);
-        telemetry.addData("gamepad1 b for out position", stopperEndPosition);
+        telemetry.addLine("Code starting");
         telemetry.update();
+        
+        //allows the wait time to include the gyro calibration to prevent any excess time wasted
+        timeOne = this.getRuntime();
+        timeTwo = this.getRuntime();
 
-        while (this.opModeIsActive()) {
-            if (gamepad1.x) {
-                ballControl.setPosition(stopperStartPosition);
-            }
-            else if (gamepad1.b) {
-                ballControl.setPosition(stopperEndPosition);
-            }
+        //calibrateGyro
+        calibrateGyro();
+
+        //the wait loop to prevent the code starting until the specified delay is finished
+        while (timeTwo - timeOne < wait) {
+            timeTwo = this.getRuntime();
         }
+
+        //shooter given power to get the motor up to speed
+        shooter.setPower(shooterPower);
+
+        //hood set to scoring position desired for shooting
+        hood.setPosition(.6);
+
+        //drive back 1 using parameters specified by driver
+        // driveBack (double distance, double speed, double targetHeading)
+        driveBack(distance1, power, heading);
+
+        //shoot using tested values of 2700 RPM
+        // shootAndLift(double targetRPM, double intakeSpeed)
+        shootAndLift(2700, .95, 5);
+
+        //drive back 2 using parameters specified by driver
+        // driveBack (double distance, double speed, double targetHeading)
+        driveBack(distance2, power, heading);
+
+        stopDriving();
+        shooter.setPower(0);
+
     }
 
-    public void setStartPosition ()
+    //this is the first action called allowing the user
+    // of Gamepad 1 to specify the time delay in seconds
+    public void setWaitTime()
     {
         stepInMenu = 1;
         choiceNotSelected = true;
-        startPositionNotSelected = true;
+        waitTimeNotSelected = true;
         telemetry.clearAll();
 
         while (choiceNotSelected && stepInMenu == 1)   {
 
-            if (startPositionNotSelected){
+            if (waitTimeNotSelected){
                 telemetry.addData("STEP NUMBER = ", stepInMenu);
                 telemetry.addLine("Gamepad 1 ONLY, START to move on");
-                telemetry.addData("Set start position", stopperStartPosition);
-                telemetry.addLine("use dpad to adjust position");
+                telemetry.addData("Set wait time in seconds","%.1f", wait);
+                telemetry.addLine("This time includes gyro calibration period");
+                telemetry.addLine("use dpad to adjust wait");
                 telemetry.update();
                 if (gamepad1.dpad_up) {
-                    stopperStartPosition += .00001;
+                    wait += .0001;
                 }
                 if (gamepad1.dpad_down) {
-                    stopperStartPosition -= .00001;
+                    wait -= .0001;
                 }
                 if (gamepad1.start) {
-                    startPositionNotSelected = false;
+                    waitTimeNotSelected = false;
                 }
+                wait = Range.clip(wait, 0, 30);
             }
 
-            if (!startPositionNotSelected) {
+            if (!waitTimeNotSelected) {
 
                 telemetry.addData("STEP NUMBER = ", stepInMenu);
                 telemetry.addLine("Gamepad 1 ONLY");
-                telemetry.addData("Confirm your start position", stopperStartPosition);
+                telemetry.addData("Confirm your wait time","%.1f", wait);
                 telemetry.addLine("Y is correct.  A is incorrect");
                 telemetry.update();
 
                 if (gamepad1.y){
                     choiceNotSelected = false;
-                    startPositionNotSelected = false;
+                    waitTimeNotSelected = false;
                     delayShort();
                 }
                 if (gamepad1.a) {
-                    startPositionNotSelected = true;
+                    waitTimeNotSelected = true;
                     delayShort();
                 }
             }
@@ -134,65 +194,69 @@ public class TuneStopper extends FunctionsForLA {
         stepInMenu = 2;
         choiceNotSelected = true;
 
+
     }
 
-    public void setEndPosition ()
+    //this is the second action called allowing the user
+    // of Gamepad 1 to specify the first distance to travel in inches
+    public void setDistance1ToTravel ()
     {
         stepInMenu = 2;
         choiceNotSelected = true;
-        endPositionNotSelected = true;
+        distance1NotSelected = true;
         telemetry.clearAll();
 
         while (choiceNotSelected && stepInMenu == 2)   {
 
-            if (endPositionNotSelected){
+            if (distance1NotSelected){
                 telemetry.addData("STEP NUMBER = ", stepInMenu);
                 telemetry.addLine("Gamepad 1 ONLY, START to move on");
-                telemetry.addData("Set end position", stopperEndPosition);
-                telemetry.addLine("use dpad to adjust position");
+                telemetry.addData("Set Target Distance 1 in inches", "%.1f", distance1);
+                telemetry.addLine("use dpad to adjust distance 1");
                 telemetry.update();
                 if (gamepad1.dpad_up) {
-                    stopperEndPosition += .00001;
+                    distance1 += .0001;
                 }
                 if (gamepad1.dpad_down) {
-                    stopperEndPosition -= .00001;
+                    distance1 -= .0001;
                 }
                 if (gamepad1.start) {
-                    endPositionNotSelected = false;
+                    distance1NotSelected = false;
                 }
+                distance1 = Range.clip(distance1, 0, 100);
             }
 
-            if (!endPositionNotSelected) {
+            if (!distance1NotSelected) {
 
                 telemetry.addData("STEP NUMBER = ", stepInMenu);
                 telemetry.addLine("Gamepad 1 ONLY");
-                telemetry.addData("Confirm your end position", stopperEndPosition);
+                telemetry.addData("Confirm your target distance 1", "%.1f", distance1);
                 telemetry.addLine("Y is correct.  A is incorrect");
                 telemetry.update();
 
-                if (gamepad1.y){
+                if (gamepad1.y) {
                     choiceNotSelected = false;
-                    endPositionNotSelected = false;
+                    distance1NotSelected = false;
                     delayShort();
                 }
                 if (gamepad1.a) {
-                    endPositionNotSelected = true;
+                    distance1NotSelected = true;
                     delayShort();
                 }
-            }
 
-            if (gamepad1.back) {
-                stepInMenu = stepInMenu - 1;
-                delayShort();
-                goBack = true;
-            }
+                if (gamepad1.back) {
+                    stepInMenu = stepInMenu - 1;
+                    delayShort();
+                    goBack = true;
+                }
 
+            }
             telemetry.update();
         }
 
         if (goBack){
             stepInMenu = 1;
-            endPositionNotSelected = true;
+            distance1NotSelected = true;
             delayShort();
         }
         else {
@@ -205,6 +269,150 @@ public class TuneStopper extends FunctionsForLA {
 
     }
 
+    //this is the third action called allowing the user
+    // of Gamepad 1 to specify the second distance to travel in inches
+    public void setDistance2ToTravel ()
+    {
+        stepInMenu = 3;
+        choiceNotSelected = true;
+        distance2NotSelected = true;
+        telemetry.clearAll();
+
+        while (choiceNotSelected && stepInMenu == 3)   {
+
+            if (distance2NotSelected){
+                telemetry.addData("STEP NUMBER = ", stepInMenu);
+                telemetry.addLine("Gamepad 1 ONLY, START to move on");
+                telemetry.addData("Set Target Distance 2 in inches", "%.1f", distance2);
+                telemetry.addLine("use dpad to adjust distance 2");
+                telemetry.update();
+                if (gamepad1.dpad_up) {
+                    distance2 += .0001;
+                }
+                if (gamepad1.dpad_down) {
+                    distance2 -= .0001;
+                }
+                if (gamepad1.start) {
+                    distance2NotSelected = false;
+                }
+                distance2 = Range.clip(distance2, 0, 100);
+            }
+
+            if (!distance2NotSelected) {
+
+                telemetry.addData("STEP NUMBER = ", stepInMenu);
+                telemetry.addLine("Gamepad 1 ONLY");
+                telemetry.addData("Confirm your target distance 2", "%.1f", distance2);
+                telemetry.addLine("Y is correct.  A is incorrect");
+                telemetry.update();
+
+                if (gamepad1.y) {
+                    choiceNotSelected = false;
+                    distance2NotSelected = false;
+                    delayShort();
+                }
+                if (gamepad1.a) {
+                    distance2NotSelected = true;
+                    delayShort();
+                }
+
+                if (gamepad1.back) {
+                    stepInMenu = stepInMenu - 1;
+                    delayShort();
+                    goBack = true;
+                }
+
+            }
+            telemetry.update();
+        }
+
+        if (goBack){
+            stepInMenu = 2;
+            distance1NotSelected = true;
+            delayShort();
+        }
+        else {
+            goBack = false;
+            stepInMenu = 4;
+            delayShort();
+        }
+        choiceNotSelected = true;
+        goBack = false;
+
+    }
+
+    //this is the fourth action called allowing the user
+    // of Gamepad 1 to specify the driving motor power
+    public void setMotorPower ()
+    {
+        stepInMenu = 4;
+        choiceNotSelected = true;
+        motorPowerNotSelected = true;
+        telemetry.clearAll();
+
+        while (choiceNotSelected && stepInMenu == 4)   {
+
+            if (motorPowerNotSelected){
+                telemetry.addData("STEP NUMBER = ", stepInMenu);
+                telemetry.addLine("Gamepad 1 ONLY, START to move on");
+                telemetry.addData("Set Motor Power", "%.2f", power);
+                telemetry.addLine("ONLY POSITIVE VALUES");
+                telemetry.addLine("use dpad to adjust power");
+                telemetry.update();
+                if (gamepad1.dpad_up) {
+                    power += .00001;
+                }
+                if (gamepad1.dpad_down) {
+                    power -= .00001;
+                }
+                if (gamepad1.start) {
+                    motorPowerNotSelected = false;
+                }
+                power = Range.clip(power, 0, 1);
+            }
+
+            if (!motorPowerNotSelected) {
+
+                telemetry.addData("STEP NUMBER = ", stepInMenu);
+                telemetry.addLine("Gamepad 1 ONLY");
+                telemetry.addData("Confirm your motor power", "%.2f", power);
+                telemetry.addLine("Y is correct.  A is incorrect");
+                telemetry.update();
+
+                if (gamepad1.y) {
+                    choiceNotSelected = false;
+                    motorPowerNotSelected = false;
+                    delayShort();
+                }
+                if (gamepad1.a) {
+                    motorPowerNotSelected = true;
+                    delayShort();
+                }
+
+                if (gamepad1.back) {
+                    stepInMenu = stepInMenu - 1;
+                    delayShort();
+                    goBack = true;
+                }
+
+            }
+            telemetry.update();
+        }
+
+        if (goBack){
+            stepInMenu = 3;
+            distance1NotSelected = true;
+            delayShort();
+        }
+        else {
+            goBack = false;
+            stepInMenu = 1;
+            delayShort();
+        }
+        choiceNotSelected = true;
+        goBack = false;
+    }
+
     public void delayShort ()
     {
         timeOne = this.getRuntime();
@@ -215,14 +423,7 @@ public class TuneStopper extends FunctionsForLA {
         }
     }
 
-    public void delayLong ()
-    {
-        timeOne = this.getRuntime();
-        timeTwo = this.getRuntime();
-
-        while (timeTwo - timeOne < 3) {
-            timeTwo = this.getRuntime();
-        }
-    }
 
 }
+
+
